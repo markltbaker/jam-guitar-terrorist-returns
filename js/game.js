484e577130df85e1e0b7fdb194518eaaebf04073
Mark Baker <markb@mindcandy.com>
2012-11-06 19:20:59 +0000

    Initial playable version of Atomic Equestrian Rocket! Doesn't have a win/end condition, needs atomic power to restore ship strength! Needs game over + timer to tell you how long you lasted

--- js/game.js
+++ js/game.js
@@ -6,210 +6,80 @@
         },
         checkOutOfBounds: function(oldPosition) {
             if(!this.within(0, 0, Crafty.viewport.width, Crafty.viewport.height)) {
-                console.log('out of bounds', this.pos(), oldPosition);
+                // console.log('out of bounds', this.pos(), oldPosition);
                 this.attr({x: oldPosition.x, y: oldPosition.y});
             }
         }
     });
     
-    //base component for Actors - i.e. Players, Enemies, NPCs etc.
-    Crafty.c('Actor', {
-        init: function() {
-            //uses the AnimationLoader component to load the sprites and data.
-            //we are reusing the sprites and data from BrowserQuest: https://github.com/mozilla/BrowserQuest
-            
-            //Delay component allows us to use the delay function to schedule timeouts that pause when
-            //the game loop pauses.
-            
-            //Collision component allows us to react to collisions with other entities
-            this.requires('AnimationLoader, Delay');
-            
-            //once the animations are loaded...
-            this.bind('AnimsLoaded', function() {
-                // set collision bounds from loaded animation bounds
-                this.requires('Collision').collision(this.boundsAsPolygon());
 
-                //set the initial animation to an idle one
-                this.animate('idle_down', 30, -1);
-            });
-        },
-        attack: function() {
-            this.triggerAnimation('atk');
-        },
-        triggerAnimation: function(type) {
-            var direction = this.direction || 'down';
-            
-            //left direction animations are just right animations that are flipped
-            if (this.direction === 'left') {
-                direction = 'right';
-            }
-            
-            var animationName = type + "_" + direction;
-            
-            if(type === 'walk') {
-                this.stop().animate(animationName, 10, -1);
-            } else if(type === 'idle') {
-                this.stop().animate(animationName, 30, -1);
-            } else if(type === 'atk') {
-                
-                //create a named function so we can unbind it using this reference later
-                function onEnd() {
-                    //delay this by one game tick as the animation is ended straight after
-                    //this callback returns.
-                    this.delay(function() {
-                        this.triggerAnimation('idle');
-                    }, 1);
-                    
-                    this.unbind('AnimationEnd', onEnd); //clean up after ourselves and unbind the event handler
-                }
-                
-                this.stop().animate(animationName, 10, 0);
-                this.bind('AnimationEnd', onEnd.bind(this)); //when the animation ends, trigger the 'onEnd' function above
-            }
-            
-            return this;
-        },
-        onNewDirection: function(direction) {
-            //Handler for the 'NewDirection' event fire by the Fourway component.
-            //Here, we record the direction and if it changes, we swap the playing animation
-            
-            if (direction.x < 0) {
-                if (this.direction !== 'left') {
-                    this.direction = 'left';
-                    this.triggerAnimation('walk').flip();
-                }
-            }
-            if (direction.x > 0) {
-                if (this.direction !== 'right') {
-                    this.direction = 'right';
-                    this.triggerAnimation('walk').unflip();
-                    
-                }
-            }
-            if (direction.y < 0) {
-                if (this.direction !== 'up') {
-                    this.direction = 'up';
-                    this.triggerAnimation('walk');
-                }
-            }
-            if (direction.y > 0) {
-                if (this.direction !== 'down') {
-                    this.direction = 'down';
-                    this.triggerAnimation('walk');
-                }
-            }
-            
-            //if direction.x and direction.y are undefined or 0, then change animation
-            //to an idle one, using the last direction we stored.
-            if(!direction.x && !direction.y) {
-                this.stop();
-                if (this.direction === 'left' || this.direction === 'right') {
-                    this.triggerAnimation('idle');
-                } else if (this.direction === 'up') {
-                    this.triggerAnimation('idle');
-                } else {
-                    this.triggerAnimation('idle');
-                }
-            }
-        }
-    });
-    
-    //Player component - because of the way that the BrowserQuest assets are rendered,
-    //we have to compose armor and weapon entities together.
-    //This component also has the behaviour for when the player attacks.
+    // Player component    
     Crafty.c('Player', {
-        init: function() {
-            this.armor = Crafty.e('Actor, Armor')
-                .animationLoader('goldenarmor');
-            
-            this.weapon = Crafty.e('Actor, Weapon')
-                .animationLoader('goldensword');
-            
-            //this component does not need Canvas or DOM components since all the rendering is
-            //done by the armor and weapon. It reacts to the arrow keys (via Fourway component)
-            //and to the space bar for attack.
-            this.requires('2D, Fourway, Keyboard, ViewportBounded')
-                .attr({w: 64, h: 64})
-                .fourway(3)
-                .bind('KeyDown', function() {
-                    if(this.isDown('SPACE')) {
-                        //space bar has been pressed
-                        this.armor.attack();
-                        this.weapon.attack();
-                        this.attack();
-                    }
-
-                })
-                .requires('Collision')
+        init: function() {           
+            this.requires('2D, DOM, Sprite, Fourway, rocket, Collision, ViewportBounded')
+                .attr({x: 64, y: 64,w:199, h:96})
+                // set the speed and controls
+                .fourway(5)
                 //create a custom hit map here:
                 .collision(new Crafty.polygon(
-                    [-10, -10],
-                    [-10, 34],
-                    [34, 34],
-                    [34, -10]
+                    [10, 40],
+                    [10, 80],
+                    [180, 80],
+                    [180, 40]
                 ));
             
-            //this will move the armor and weapon entities in lockstep with the player component
-            this.attach(this.armor, this.weapon);
-            
-            //start at position [64,64]
-            this.attr({x: 64, y: 64});
-            
             //bind our movement handler to the NewDirection event
-            this.bind('NewDirection', function(direction) {
-                this.armor.onNewDirection(direction);
-                this.weapon.onNewDirection(direction);
-            });
-            
             this.bind('Moved', function(oldPosition) {
                 this.checkOutOfBounds(oldPosition);
             });
-        },
-        attack: function() {
-            //check whether we are colliding with an Enemy component
-            var hit = this.weapon.hit('Enemy');
-            if(hit) {
-                console.log('hit enemy', hit);
-                this.trigger('HitEnemy', hit.length);
-            }
         }
     });
     
-    Crafty.c('Enemy', {
+    // Space Junk component - player must dodget this!
+    Crafty.c('SpaceJunk', {
         init: function() {
-            this.requires('2D, Delay, Tween, Actor');
-            this.bind('TweenEnd', this.onMoveEnd);
-            this.delay(this.move, 2000);
+            this.requires('2D, DOM, Sprite')                
+                .bind("EnterFrame", this._updateJunk);
         },
-        move: function() {
-            var hit = this.hit('Player');
-            if (hit) {
-                this.attack();
-                this.trigger('HitPlayer');
+
+        spaceJunk: function(type) {            
+            this.requires(type)
+                .origin("center");
+            var x = this.x + 10;
+            var y = this.y + 10;
+            var x2 = this.x + this.w - 20;
+            var y2 = this.y + this.h - 20;
+
+            this.requires("Collision")               
+                .collision(new Crafty.polygon([x,y],[x,y2],[x2,y2],[x2,y]));
+
+            //this.requires("WiredHitBox");
+            this._randomlyPosition();
+        },
+
+        _randomlyPosition: function() {
+            this.rotationSpeed = Crafty.math.randomNumber(-5,5);
+            this.speed = Crafty.math.randomNumber(1,10);
+            this.attr({x: 1024, y: Crafty.math.randomInt(0,600-this.h)});
+        },
+
+        _updateJunk: function() {
+            this.rotation += this.rotationSpeed;
+            this.x -= this.speed            
+
+            if ((this.x + this.w) < 0) {
+                this._randomlyPosition();
             } else {
-                var xMovement = Crafty.math.randomInt(-100, 100);
-                var yMovement = Crafty.math.randomInt(-100, 100);
 
-                var newPos = {
-                    x: this.x + xMovement,
-                    y: this.y + yMovement,
-                    w: this.w,
-                    h: this.h
-                };
-
-                if(this.within.call(newPos, 0, 0, Crafty.viewport.width, Crafty.viewport.height)) {
-                    this.onNewDirection({x: xMovement, y: yMovement});
-                    this.tween({x: newPos.x, y: newPos.y}, 60);
+                var hit = this.hit("Player");
+                if (hit) {
+                    this.trigger('HitPlayer');
                 }
             }
-            
-            this.delay(this.move, 2000);
-        },
-        onMoveEnd: function() {
-            this.onNewDirection({x: 0, y: 0});
         }
     });
-    
+
+    // A component to display the player's score
     Crafty.c('Score', {
         init: function() {
             this.score = 0;
@@ -229,6 +99,11 @@
             this.text(this._textGen);
         }
     })
+
+
+    //
+    // Game loading and initialisation
+    //
     
     var Game = function() {
         Crafty.scene('loading', this.loadingScene);
@@ -241,7 +116,9 @@
         Crafty.canvas.init();
         
         Crafty.modules({ 'crafty-debug-bar': 'release' }, function () {
-            Crafty.debugBar.show();
+            if (Crafty.debugBar) {
+               Crafty.debugBar.show();
+            }
         });
     };
     
@@ -251,10 +128,14 @@
         loading.text('loading...');
         
         function onLoaded() {
+            Crafty.sprite('img/rocket.png', {rocket: [0, 0, 198, 96] });
+            Crafty.sprite('img/rock.png', {asteroid: [0, 0, 198, 187] });
+            Crafty.sprite('img/atom.png', {fuel: [0, 0, 198, 158] });
+            Crafty.sprite('img/satellite.png', {satellite: [0,0, 198, 176] });
+
             loading.delay(function() {
                 Crafty.scene('main');
             }, 500);
-            
         }
         
         function onProgress(progress) {
@@ -266,40 +147,37 @@
         }
         
         Crafty.load([
-            'img/boss.png',
-            'img/deathknight.png',
-            'img/goldenarmor.png',
-            'img/goldensword.png'
+            'img/atom.png',
+            'img/rock.png',
+            'img/rocket.png',
+            'img/satellite.png'
         ], 
         onLoaded, onProgress, onError);
         
     };
     
+    //
+    // The main game scene
+    //
     Game.prototype.mainScene = function() {
         //create a player...
         var player = Crafty.e('Player');
         
-        //and an enemy or two
-        var enemy = Crafty.e('Actor, Enemy,')
-            .attr({x: 350, y: 100})
-            .animationLoader('deathknight');
-        
-        var boss = Crafty.e('Actor, Boss, Enemy') //here, Boss and Enemy are just tags - we can use them
-                                                  //when checking for collisions
-                        //render at [500, 500]
-                        .attr({x: 500, y: 500})
-                        
-                        //this loads up the sprite data
-                        .animationLoader('boss');
+        var spaceJunk = [
+            Crafty.e('SpaceJunk').spaceJunk('asteroid'),
+            Crafty.e('SpaceJunk').spaceJunk('asteroid'),
+            Crafty.e('SpaceJunk').spaceJunk('satellite'),
+            ];
+
         
         var score = Crafty.e('Score');
         
-        player.bind('HitEnemy', function() {
-            score.increment();
-        });
+        // player.bind('HitEnemy', function() {
+        //     score.increment();
+        // });
         
-        Crafty('Enemy').bind('HitPlayer', function() {
-            score.decrement();
+        Crafty('SpaceJunk').bind('HitPlayer', function() {
+             score.decrement();
         });
     };
     
