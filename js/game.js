2c8aa903551a84db96fed2882744d8c813f825e5
Mark Baker <markb@mindcandy.com>
2012-11-07 16:37:03 +0000

    removed unnecessary Sprite component (it was already being added automatically)

--- js/game.js
+++ js/game.js
@@ -7,7 +7,7 @@
     Crafty.c('Renderable', {
         init: function() {
             // we're using DOM Spirtes
-            this.requires('2D, DOM, Sprite');
+            this.requires('2D, DOM');
         },
         // set which sprite to use -- should match up with a call to Crafty.sprite()
         spriteName: function(name) {
